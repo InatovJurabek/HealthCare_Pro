6f6b97f7b386cb4cf5fb1a1b07aaa24622f00179
Jurabek <inatovjorabek10@gmail.com>
2026-06-12 19:43:06 +0500

    add gitignore file

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+// const authToutes = require(".rotes/authRotes");
+// const patientToutes = require("/routes/patientRoutes");
+// const doctorRoutes = require(".routes/doctorRoutes");
+// const appointmentRoutes = require(",routes/appointmentRoutes");
+
+dotenv.config();
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
-app.get("/api/data", (req, res) => {
-  const data = {
-    message: "This is some sample data from the backend.",
-  };
-  res.json(data);
-});
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log("Server is running on port: " + PORT);
 });
